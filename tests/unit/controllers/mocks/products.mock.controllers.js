47be6b94bc27a93d5f1604746dd1f2af0a49ecb0
kayckhirt <kayckhirt15@gmail.com>
2023-01-18 16:55:33 -0300

    feat: mocks e tests Controller

---
+++ tests/unit/controllers/mocks/products.mock.controllers.js
@@ -0,0 +1,11 @@
+const productMock = {
+  id:1,
+  name: 'Martelo de Thor',
+};
+
+const productListMock = [productMock];
+
+module.exports = {
+  productMock,
+  productListMock,
+};
--- tests/unit/controllers/products.controllers.test.js
+++ tests/unit/controllers/products.controllers.test.js
@@ -1,9 +1,32 @@
-const { expect } = require('chai');
+const chai = require('chai');
 const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
+const { expect } = chai;
+chai.use(sinonChai);
 
+const productsService = require('../../../src/services/productsService');
+const productsController = require('../../../src/controllers/productsController');
+const { productListMock } = require('./mocks/products.mock.controllers');
 
-describe('', function () {
-  it('', async function (){
+describe('Teste de unidade do productsControllers', function () {
+  describe('Listando os Produtos', function () {
+    it('Deve retornar o status 200 e a lista de produtos', async function (){
+      //Arranjo
+      const res = {};
+      const req = {};
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productsService, 'getAll')
+      .resolves({ type: null, message:productListMock });
+      //AÇÃO
+      await productsController.getAll(req, res);
+      //ASSERÇÃO
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(productListMock);
+    });
+  });
+  afterEach(function () {
+    sinon.restore();
   });
 });
